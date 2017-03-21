d10a9b0d5e0e30c09d912a8b8924a856e71bc967
madlevel <yosiul@me.com>
2017-03-21 15:32:26 +0100

    updates inicio + comienzo rooms.html

---
+++ js/semantic.js
@@ -0,0 +1,17 @@
+$(function () {
+    $('a.item.navbar').on('click', function () {
+        
+        $('a.item.navbar').removeClass('active');
+        $(this).addClass('active');
+    });
+    
+    
+    // Des-oculto menú plegado en vertical.
+    $('.right.menu.open').on("click", function(e){
+        e.preventDefault();
+		$('.ui.vertical.menu').toggle();
+	});
+        
+	$('.ui.dropdown').dropdown();
+
+})
